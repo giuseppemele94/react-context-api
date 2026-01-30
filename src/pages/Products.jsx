04edec1e002b61fee1b353288830956dcf7ad77a
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-30 17:09:57 +0100

    Filtraggio prodotti per prezzo tramite stato globale budgetMode

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import ProductList from "../components/ProductList"
 import { useEffect, useState } from "react";
+import { useBudget } from "../contexts/BudgetContext";
+
 
 function Products() {
 
     //var di stato che conterrà la lista dei prodotti 
     const [products, setProducts] = useState([]);
     const endpoint = "https://fakestoreapi.com/products";
+
+    //leggo lo stato globale dal context
+    const { budgetMode } = useBudget();
 
     //montaggio del componente al cambio di Route 
     useEffect(() => {
@@ -21,10 +26,16 @@ function Products() {
             })
     }, []); //viene eseguito una sola volta quando entro nella pagina 
 
+
+    //quindi se budget mode === true stampo un array di oggetti filtrato in base al prezzo <=30
+    //altrimenti stampo products
+
+    const filterProducts = budgetMode ? products.filter(product => product.price <= 30)
+        : products;
     return (
-       
-            <ProductList products={products} />
-        
+
+        <ProductList products={filterProducts} />
+
     )
 }
 
